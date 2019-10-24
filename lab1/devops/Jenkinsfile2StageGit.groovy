509943b8bd30b281a2abeddae12566f175fa7eb2
JasonNP <jj9_35@hotmail.com>
2019-10-24 23:28:17 +0000

    sh ls -lta

--- lab1/devops/Jenkinsfile2StageGit.groovy
+++ lab1/devops/Jenkinsfile2StageGit.groovy
@@ -4,12 +4,19 @@ node{
         withEnv(["ENV_CLIENTID=123456789-123456-123456"]){
             stage("init"){
                 echo "${ENV_CLIENTID}"
+                sh "printenv"
             }
 
         }
         
         stage("checkout"){
-            println "todo"
+            println "===========TODO==========="
+            git(
+                branch : master,
+                credentialsId: "jason-github",
+                url: "https://github.com/jasonNP/aks-rbac-example.git"
+            )
+            sh "ls -lta"
         }
     }
 
