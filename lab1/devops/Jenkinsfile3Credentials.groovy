6a75dd77be7cba8278d250e6886fc11a6d556a97
JasonNP <jj9_35@hotmail.com>
2019-10-25 00:01:53 +0000

    j

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -1,4 +1,22 @@
 
 node{
-    
+    stage("init"){
+
+    }
+    stage("checkout"){
+        withCredentials([string(credentialsId:"jason-token",variable:"token1")]){
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok2 = tokentmp[1]
+
+            echo " ${tok1} ---- ${tok2}"
+        }
+
+    }
 }
+
+/*
+password2019|jj9_35@hotmail.com|123456
+*/
